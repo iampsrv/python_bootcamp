58aeb4bee47ce793f8e4e0d78183c1bdece8e692
iampsrv <iampsrv@gmail.com>
2024-01-30 22:24:53 +0530

    python var

---
+++ first_milestone/variable.py
@@ -0,0 +1,16 @@
+# Variable tutorial
+
+name = "Pranjal Srivastava" # strings variables
+age = "26"  # integer/number variable
+skills = ["python", "java", "c"] # list variable
+
+a1997 = "number"
+print1 = "hello"
+print(type(age))
+age=int(age)
+print(type(age))
+# print(name)
+# print(age)
+
+# name = "Pranjal"
+# print(name)
